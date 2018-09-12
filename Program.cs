b51c061107ed6cbcadc403443b24410fbf39c2a0
Luc Leysen <luc56.leysen@gmail.com>
2018-09-12 21:08:26 +0200

    read metadata

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
 
 namespace VCCode_Mac
 {
@@ -11,8 +14,23 @@ namespace VCCode_Mac
             {
                 var metadataFilePath = Path.Combine(subfolder, "metadata.json");
                 Console.WriteLine($"Reading {metadataFilePath}");
-                
+
+                var metadataCollection = GetMetadata(metadataFilePath);
             }
+        }
+
+        private static List<Metadata> GetMetadata(string metadataFilePath)
+        {
+            var settings = new DataContractJsonSerializerSettings
+            {
+                DateTimeFormat = new DateTimeFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+            };
+            var serializer = new DataContractJsonSerializer(typeof(List<Metadata>), settings);
+
+            using (var metadataFileStream = File.Open(metadataFilePath, FileMode.Open))
+            {
+                 return (List<Metadata>)serializer.ReadObject(metadataFileStream);
+            };
         }
     }
 }
