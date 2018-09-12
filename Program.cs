a5d06d74f267b8e2312f0f1c372199e1dba7c0a3
Luc Leysen <luc56.leysen@gmail.com>
2018-09-12 19:49:05 +0200

    initial project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VCCode_Mac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
